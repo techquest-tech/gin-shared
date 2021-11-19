cfdccdf96b3fe10be2b1902505398962acaa3e08
armenpn <armenpn@gmail.com>
2021-11-19 20:25:54 +0800

    trial keycloak

--- demo/demo.go
+++ demo/demo.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
+	"github.com/tbaehler/gin-keycloak/pkg/ginkeycloak"
 	"github.com/techquest-tech/gin-shared/pkg/ginshared"
 )
 
@@ -18,11 +19,32 @@ func (d *DemoController) Hello(c *gin.Context) {
 	c.JSON(200, gin.H{"hello": "world"})
 }
 
+func (d *DemoController) Admin(c *gin.Context) {
+	c.JSON(200, gin.H{"hello": "Admin"})
+}
+
 // NewDemoController must return ginshared.DiController --- Step 1
 func NewDemoController(router *gin.Engine, logger *zap.Logger) ginshared.DiController {
+
+	var sbbEndpoint = ginkeycloak.KeycloakConfig{
+		Url:   "https://sso.sit-k8s.esquel.cn/",
+		Realm: "rfid",
+	}
+
 	controller := &DemoController{}
-	router.GET("/healthz", controller.Hello)
+	router.Use(ginkeycloak.Auth(ginkeycloak.AuthCheck(), sbbEndpoint)).GET("/healthz", controller.Hello)
 	logger.Info("controller is ready.")
+
+	config := ginkeycloak.BuilderConfig{
+		Service: "hazzys",
+		Url:     "https://sso.sit-k8s.esquel.cn/",
+		Realm:   "rfid",
+	}
+
+	auth := ginkeycloak.NewAccessBuilder(config).RestrictButForRole("admin").Build()
+
+	router.Group("/admin").Use(auth).GET("/hello", controller.Admin)
+
 	return controller
 }
 
--- go.mod
+++ go.mod
@@ -9,10 +9,11 @@ require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/zap v0.0.1
 	github.com/gin-gonic/gin v1.7.4
-	github.com/go-playground/validator/v10 v10.4.1
+	github.com/go-playground/validator/v10 v10.9.0
 	github.com/spf13/viper v1.9.0
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
 	github.com/swaggo/gin-swagger v1.3.2
+	github.com/tbaehler/gin-keycloak v1.2.1
 	go.uber.org/dig v1.13.0
 	go.uber.org/zap v1.19.1
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
