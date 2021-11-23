7876f535b7e892cd638c317a1533051091518bec
armenpn <armenpn@gmail.com>
2021-11-23 17:54:40 +0800

    trial keycloak & upgrade to 1.17

--- demo/demo.go
+++ demo/demo.go
@@ -4,11 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
-	"github.com/tbaehler/gin-keycloak/pkg/ginkeycloak"
+	"github.com/techquest-tech/gin-shared/pkg/auth"
 	"github.com/techquest-tech/gin-shared/pkg/ginshared"
 )
 
 type DemoController struct {
+	logger *zap.Logger
 }
 
 // func (d *DemoController) GetControllerName() string {
@@ -16,34 +17,45 @@ type DemoController struct {
 // }
 
 func (d *DemoController) Hello(c *gin.Context) {
+	currently, ok := c.Get("token")
+	if ok {
+		d.logger.Info("user infor", zap.Any("current", currently))
+	}
+
 	c.JSON(200, gin.H{"hello": "world"})
 }
 
 func (d *DemoController) Admin(c *gin.Context) {
+
 	c.JSON(200, gin.H{"hello": "Admin"})
 }
 
 // NewDemoController must return ginshared.DiController --- Step 1
-func NewDemoController(router *gin.Engine, logger *zap.Logger) ginshared.DiController {
+func NewDemoController(router *gin.Engine, logger *zap.Logger, keycloak *auth.KeycloakConfig) ginshared.DiController {
 
-	var sbbEndpoint = ginkeycloak.KeycloakConfig{
-		Url:   "https://sso.sit-k8s.esquel.cn/",
-		Realm: "rfid",
+	// var sbbEndpoint = ginkeycloak.KeycloakConfig{
+	// 	Url:   "https://sso.sit-k8s.esquel.cn/",
+	// 	Realm: "rfid",
+	// }
+
+	controller := &DemoController{
+		logger: logger,
 	}
 
-	controller := &DemoController{}
-	router.Use(ginkeycloak.Auth(ginkeycloak.AuthCheck(), sbbEndpoint)).GET("/healthz", controller.Hello)
-	logger.Info("controller is ready.")
+	// router.Use(ginkeycloak.Auth(ginkeycloak.AuthCheck(), sbbEndpoint)).GET("/healthz", controller.Hello)
+	// logger.Info("controller is ready.")
 
-	config := ginkeycloak.BuilderConfig{
-		Service: "hazzys",
-		Url:     "https://sso.sit-k8s.esquel.cn/",
-		Realm:   "rfid",
-	}
+	// config := ginkeycloak.BuilderConfig{
+	// 	Service: "hazzys",
+	// 	Url:     "https://sso.sit-k8s.esquel.cn/",
+	// 	Realm:   "rfid",
+	// }
 
-	auth := ginkeycloak.NewAccessBuilder(config).RestrictButForRole("admin").Build()
+	// x := ginkeycloak.NewAccessBuilder(config)
+	// auth := x.RestrictButForRealm("admin").Build()
 
-	router.Group("/admin").Use(auth).GET("/hello", controller.Admin)
+	router.Group("/private").Use(keycloak.Auth()).GET("/hello", controller.Admin)
+	router.Group("/api").Use(keycloak.Auth()).GET("/hello", controller.Hello)
 
 	return controller
 }
--- pkg/auth/keycloak.go
+++ pkg/auth/keycloak.go
@@ -1,1 +1,49 @@
 package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"github.com/tbaehler/gin-keycloak/pkg/ginkeycloak"
+	"github.com/techquest-tech/gin-shared/pkg/ginshared"
+	"go.uber.org/zap"
+)
+
+func init() {
+	ginshared.GetContainer().Provide(NewKeycloakConfig)
+}
+
+type KeycloakConfig struct {
+	DefaultRoles []string
+	BuildConfig  ginkeycloak.BuilderConfig
+}
+
+func (kc *KeycloakConfig) Auth(roles ...string) gin.HandlerFunc {
+	if len(roles) == 0 {
+		roles = kc.DefaultRoles
+	}
+	x := ginkeycloak.NewAccessBuilder(kc.BuildConfig)
+	for _, role := range roles {
+		x = x.RestrictButForRealm(role)
+	}
+	return x.Build()
+}
+
+func NewKeycloakConfig(logger *zap.Logger) *KeycloakConfig {
+	config := &KeycloakConfig{}
+
+	settings := viper.Sub("keycloak")
+
+	if settings == nil {
+		logger.Error("keycloak settings are missing.")
+		return nil
+	}
+
+	buildconfig := ginkeycloak.BuilderConfig{}
+	settings.Unmarshal(&buildconfig)
+	config.BuildConfig = buildconfig
+	config.DefaultRoles = settings.GetStringSlice("roles")
+
+	logger.Info("load keycloak config", zap.Any("config", config))
+
+	return config
+}
