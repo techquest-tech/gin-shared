52ce3a9eb0cf3013284556b439ca6690beb935e8
armenpn <armenpn@gmail.com>
2022-05-30 23:07:50 +0800

    save request body & resp status to DB.

---
+++ pkg/tracing/todb.go
@@ -0,0 +1,65 @@
+package tracing
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"github.com/techquest-tech/gin-shared/pkg/orm"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+type FullRequestDetails struct {
+	gorm.Model
+	Body   string
+	Status int
+}
+
+type TracingRequestService struct {
+	DB     *gorm.DB
+	Logger *zap.Logger
+}
+
+func NewTracingRequestService(db *gorm.DB, logger *zap.Logger) (*TracingRequestService, error) {
+	tr := &TracingRequestService{
+		DB:     db,
+		Logger: logger,
+	}
+	if viper.GetBool(orm.KeyInitDB) {
+		err := db.AutoMigrate(&FullRequestDetails{})
+		if err != nil {
+			logger.Error("create fullRequestDetals failed.", zap.Error(err))
+		} else {
+			logger.Info("create fullRequestDetails table done")
+		}
+	}
+	return tr, nil
+}
+
+func (tr *TracingRequestService) LogfullRequestDetails(c *gin.Context) {
+	data := make([]byte, 0)
+
+	if c.Request.Body != nil {
+		data, _ = ioutil.ReadAll(c.Request.Body)
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	}
+
+	c.Next()
+	status := c.Writer.Status()
+	go tr.doLogRequestBody(data, status)
+}
+
+func (tr *TracingRequestService) doLogRequestBody(data []byte, status int) {
+	req := FullRequestDetails{
+		Body:   string(data),
+		Status: status,
+	}
+	err := tr.DB.Save(&req).Error
+	if err != nil {
+		tr.Logger.Error("save reqest failed", zap.Error(err))
+		return
+	}
+	tr.Logger.Info("save request details done.")
+}
