451ca901b0b343ccc79e738a1513ce031920f54f
liangliz <liangliz@esquel.com>
2022-06-02 15:46:40 +0800

    增加发送email通用模块

--- go.mod
+++ go.mod
@@ -12,11 +12,13 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator/v10 v10.10.1
 	github.com/google/uuid v1.3.0
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/viper v1.10.1
 	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
 	github.com/swaggo/gin-swagger v1.4.1
 	github.com/tbaehler/gin-keycloak v1.3.0
+	github.com/xhit/go-str2duration/v2 v2.0.0
 	go.uber.org/dig v1.14.0
 	go.uber.org/zap v1.21.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
@@ -82,4 +84,5 @@ require (
 	gopkg.in/ini.v1 v1.66.3 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 )
---
+++ pkg/email/smtp.go
@@ -0,0 +1,114 @@
+package email
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/smtp"
+
+	"github.com/jordan-wright/email"
+	"go.uber.org/zap"
+)
+
+
+type SmtpSettings struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	auth     smtp.Auth
+}
+
+type EmailTmpl struct {
+	Subject string
+	Body    string
+	Notfound string
+	Receivers []string
+	tSub    *template.Template
+	tBody   *template.Template
+	tNotfound *template.Template
+}
+
+type EmailNotifer struct {
+	Logger    *zap.Logger
+	From      string
+	//Receivers []string
+	SMTP      SmtpSettings
+	Template  map[string]*EmailTmpl
+}
+
+func (en *EmailNotifer) PostInit() error {
+	for _, item := range en.Template {
+		item.tSub = template.Must(template.New("sub").Parse(item.Subject))
+		item.tBody = template.Must(template.New("body").Parse(item.Body))
+		item.tNotfound = template.Must(template.New("body").Parse(item.Notfound))
+
+		en.Logger.Info("template's receivers", zap.Any("template",item))
+	}
+
+	en.Logger.Debug("template is ready")
+	if en.SMTP.Username != "" {
+		en.SMTP.auth = smtp.PlainAuth("", en.SMTP.Username, en.SMTP.Password, en.SMTP.Host)
+	}
+	return nil
+}
+
+func (en *EmailNotifer) Send(tmpl string, data map[string]interface{}, attachments ...string) error {
+	e := email.NewEmail()
+
+	e.From = en.From
+	//e.To = en.Receivers
+
+	out := bytes.Buffer{}
+
+	tmp := en.Template[tmpl]
+	en.Logger.Info("template", zap.String("tmpl", tmpl))
+	en.Logger.Info("template is ", zap.Any("", tmp))
+	e.To = tmp.Receivers
+
+	err := tmp.tSub.Execute(&out, data)
+	if err != nil {
+		en.Logger.Error("match email subject failed.", zap.Error(err))
+		return err
+	}
+
+	e.Subject = out.String()
+
+	out = bytes.Buffer{}
+
+	if attachments != nil {
+		err = tmp.tBody.Execute(&out, data)
+		if err != nil {
+			en.Logger.Error("match email content failed.", zap.Error(err))
+			return err
+		}
+	} else {
+		err = tmp.tNotfound.Execute(&out, data)
+		if err != nil {
+			en.Logger.Error("match email content failed.", zap.Error(err))
+			return err
+		}
+	}
+
+	e.HTML = out.Bytes()
+
+	for _, file := range attachments {
+		_, err = e.AttachFile(file)
+		if err != nil {
+			en.Logger.Error("attach file failed", zap.String("file", file), zap.Error(err))
+			return err
+		}
+		en.Logger.Info("attached file", zap.String("file", file))
+	}
+	fullAddress := fmt.Sprintf("%s:%d", en.SMTP.Host, en.SMTP.Port)
+
+	en.Logger.Debug("start to send email", zap.String("smtp", fullAddress), zap.Strings("receivers", tmp.Receivers))
+
+	err = e.Send(fullAddress, en.SMTP.auth)
+	if err != nil {
+		en.Logger.Error("send email failed", zap.Error(err))
+		return err
+	}
+	en.Logger.Info("send email done.", zap.Strings("receivers", tmp.Receivers))
+	return nil
+}
