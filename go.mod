2ab65a6884048d220ded8449b3d2383c7a19d2f8
panarm <panarm@esquel.com>
2022-09-19 14:34:18 +0800

    add mqtt client

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/Depado/ginprom v1.7.4
 	github.com/asaskevich/EventBus v0.0.0-20200907212545-49d423059eef
+	github.com/eclipse/paho.mqtt.golang v1.4.1
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-contrib/zap v0.0.2
 	github.com/gin-gonic/gin v1.8.1
@@ -22,6 +23,11 @@ require (
 	go.uber.org/zap v1.22.0
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.8
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 )
 
 require (
---
+++ pkg/mqttclient/mqtt.go
@@ -0,0 +1,93 @@
+package mqttclient
+
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/spf13/viper"
+	"github.com/techquest-tech/gin-shared/pkg/core"
+	"go.uber.org/zap"
+)
+
+type MqttService struct {
+	Endpoint string
+	User     string
+	Password string
+	ClientID string
+	Qos      byte
+	subs     map[string]mqtt.MessageHandler
+	Logger   *zap.Logger
+	Client   mqtt.Client
+}
+
+func init() {
+	core.GetContainer().Provide(InitMqtt)
+}
+
+func (m *MqttService) Sub(topic string, handle mqtt.MessageHandler) {
+	if m.subs == nil {
+		m.subs = make(map[string]mqtt.MessageHandler)
+	}
+	if _, ok := m.subs[topic]; !ok {
+		m.subs[topic] = handle
+	}
+
+	token := m.Client.Subscribe(topic, m.Qos, handle)
+	select {
+	case <-token.Done():
+		err := token.Error()
+		m.Logger.Error("sub topic failed", zap.String("topic", topic), zap.Error(err))
+	default:
+		m.Logger.Info("sub topic done", zap.String("topic", topic))
+	}
+
+}
+
+func (m *MqttService) OnConnect(c mqtt.Client) {
+	m.Logger.Info("connected to mqtt", zap.String("endpoint", m.Endpoint))
+	for topic, handle := range m.subs {
+		go m.Sub(topic, handle)
+	}
+}
+
+func (m *MqttService) LogMessage(c mqtt.Client, msg mqtt.Message) {
+
+	m.Logger.Info("received message", zap.String("ID", m.ClientID), zap.String("topic", msg.Topic()))
+	m.Logger.Info("message body", zap.String("msg", string(msg.Payload())))
+}
+
+func InitMqtt(logger *zap.Logger) (*MqttService, error) {
+	broke := &MqttService{
+		Endpoint: "tcp://127.0.0.1:1883",
+		Logger:   logger,
+		subs:     make(map[string]mqtt.MessageHandler),
+	}
+
+	settings := viper.Sub("mqtt")
+	if settings != nil {
+		settings.Unmarshal(broke)
+	}
+
+	opts := mqtt.NewClientOptions().AddBroker(broke.Endpoint).SetClientID(broke.ClientID)
+
+	opts.OnConnectionLost = func(c mqtt.Client, err error) {
+		logger.Error("connection lost", zap.Error(err))
+	}
+	opts = opts.SetAutoReconnect(true)
+
+	if broke.User != "" {
+		opts.Username = broke.User
+		opts.Password = broke.Password
+	}
+	opts.OnConnect = broke.OnConnect
+
+	c := mqtt.NewClient(opts)
+
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		logger.Error("connect to mqtt failed.", zap.String("endpoint", broke.Endpoint), zap.Error(token.Error()))
+		return nil, token.Error()
+	}
+
+	broke.Client = c
+
+	logger.Debug("mqtt init done.", zap.String("endpoint", broke.Endpoint))
+	return broke, nil
+}
