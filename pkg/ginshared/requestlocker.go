97959f7e3c2efbe367955099ba0228e8328fc5e9
PanArm <panarm@esquel.com>
2022-12-07 11:29:25 +0800

    request locker done.

---
+++ pkg/ginshared/requestlocker.go
@@ -0,0 +1,30 @@
+package ginshared
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func RequestLocker(params ...string) gin.HandlerFunc {
+	cache := &sync.Map{}
+	return func(ctx *gin.Context) {
+		key := ""
+		for _, item := range params {
+			key = key + ctx.Param(item)
+		}
+		_, ok := cache.LoadOrStore(key, true)
+		defer cache.Delete(key)
+		if ok {
+			zap.L().Warn("another request is processing, this request will be ignored.")
+			ctx.JSON(http.StatusTooManyRequests, gin.H{
+				"error": "another request is processing",
+			})
+			ctx.Abort()
+			return
+		}
+		ctx.Next()
+	}
+}
