4a1dfa50892e5ce8a4f88c8559a9d90bfca7a82b
PanArm <panarm@esquel.com>
2023-06-26 23:02:58 +0800

    add monitor to httpclient.

--- pkg/untils/httpclient/httpclient.go
+++ pkg/untils/httpclient/httpclient.go
@@ -4,17 +4,21 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/avast/retry-go"
+	"github.com/techquest-tech/gin-shared/pkg/core"
+	"github.com/techquest-tech/gin-shared/pkg/event"
+	"github.com/techquest-tech/monitor"
 	"go.uber.org/zap"
 )
 
-func RequestWithRetry(req *http.Request, result interface{}) error {
+func RequestWithRetry(req *http.Request, result interface{}, body ...string) error {
 	log := zap.L().With(zap.String("service", "clientWithRetry"))
 
 	client := &http.Client{}
 	err := retry.Do(func() error {
-		// log.Info("request to upstream", zap.String("endpoint", url))
+		start := time.Now()
 		resp, err := client.Do(req)
 		if err != nil {
 			return err
@@ -22,6 +26,22 @@ func RequestWithRetry(req *http.Request, result interface{}) error {
 		defer resp.Body.Close()
 
 		cached, err := io.ReadAll(resp.Body)
+
+		if event.Bus != nil && len(body) > 0 {
+			fulllogging := &monitor.TracingDetails{
+				Optionname: req.URL.String(),
+				Uri:        req.URL.String(),
+				Method:     req.Method,
+				Body:       body[0],
+				Durtion:    time.Since(start),
+				Status:     resp.StatusCode,
+				Resp:       string(cached),
+				App:        core.AppName,
+				Version:    core.Version,
+			}
+			event.Bus.Publish(event.EventTracing, fulllogging)
+		}
+
 		if err != nil {
 			return err
 		}
@@ -34,6 +54,7 @@ func RequestWithRetry(req *http.Request, result interface{}) error {
 			return err
 		}
 		log.Info("request upstream done.", zap.Any("result", result))
+
 		return nil
 	})
 	return err
