9886e72037844474afe57203bf79fdd2c4a72c85
PanArm <panarm@esquel.com>
2024-07-01 15:28:28 +0800

    make CacheRam for service cache.

--- pkg/cache/defaultRam.go
+++ pkg/cache/defaultRam.go
@@ -14,20 +14,3 @@ func NewCacheProvider[T any](t time.Duration) CacheProvider[T] {
 	}
 	return rr
 }
-
-type CacheRam[T any] struct {
-	ram cache.Cache
-}
-
-func (cc *CacheRam[T]) Set(key string, value T) {
-	cc.ram.SetDefault(key, value)
-}
-
-func (cc *CacheRam[T]) Get(key string) (T, bool) {
-	if vv, found := cc.ram.Get(key); found {
-		v := vv.(T)
-		return v, true
-	}
-	var vv T
-	return vv, false
-}
---
+++ pkg/cache/ram.go
@@ -0,0 +1,31 @@
+package cache
+
+import (
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+type CacheRam[T any] struct {
+	ram cache.Cache
+}
+
+func (cc *CacheRam[T]) Set(key string, value T) {
+	cc.ram.SetDefault(key, value)
+}
+
+func (cc *CacheRam[T]) Get(key string) (T, bool) {
+	if vv, found := cc.ram.Get(key); found {
+		v := vv.(T)
+		return v, true
+	}
+	var vv T
+	return vv, false
+}
+
+func NewRAMCacheProvider[T any](t time.Duration) *CacheRam[T] {
+	rr := &CacheRam[T]{
+		ram: *cache.New(t, 2*t),
+	}
+	return rr
+}
