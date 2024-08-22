93f5e77066ff95d721530a51573b8c0e0231c7e4
PanArm <panarm@esquel.com>
2024-08-22 17:56:26 +0800

    bug fixed.

--- pkg/cache/defaultRam.go
+++ pkg/cache/defaultRam.go
@@ -11,7 +11,7 @@ import (
 	"github.com/techquest-tech/gin-shared/pkg/core"
 )
 
-func NewCacheProviderT[T any](t time.Duration) CacheProvider[T] {
+func NewCacheProvider[T any](t time.Duration) CacheProvider[T] {
 	rr := &CacheRam[T]{
 		ram: *cache.New(t, 2*t),
 	}
