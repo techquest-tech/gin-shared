6d6dfdd33e94b759e74fa093085618cdced09d33
PanArm <panarm@esquel.com>
2024-08-22 17:52:51 +0800

    update ram Hash.

--- pkg/cache/defaultRam.go
+++ pkg/cache/defaultRam.go
@@ -3,14 +3,44 @@
 package cache
 
 import (
+	"context"
+	"sync"
 	"time"
 
 	"github.com/patrickmn/go-cache"
+	"github.com/techquest-tech/gin-shared/pkg/core"
 )
 
-func NewCacheProvider[T any](t time.Duration) CacheProvider[T] {
+func NewCacheProviderT[T any](t time.Duration) CacheProvider[T] {
 	rr := &CacheRam[T]{
 		ram: *cache.New(t, 2*t),
 	}
 	return rr
+}
+
+type LocalRamHash struct {
+	ram sync.Map
+}
+
+func (lhash *LocalRamHash) GetValues(ctx context.Context, key string, fields ...string) ([]any, error) {
+	result := make([]any, len(fields))
+	for index, item := range fields {
+		if vv, found := lhash.ram.Load(key + item); found {
+			result[index] = vv
+		}
+	}
+	return result, nil
+}
+func (lhash *LocalRamHash) SetValues(ctx context.Context, key string, values map[string]string) error {
+	for k, v := range values {
+		lhash.ram.Store(key+k, v)
+	}
+	return nil
+}
+
+func NewLocalRamHash() Hash {
+	return &LocalRamHash{}
+}
+func init() {
+	core.Provide(NewLocalRamHash)
 }
