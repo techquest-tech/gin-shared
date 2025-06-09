b6715778e52625185e06dbbcb35e3bdd042fda32
PanArm <panarm@esquel.com>
2025-06-09 17:44:36 +0800

    raw query refactor.

--- pkg/query/rawquery.go
+++ pkg/query/rawquery.go
@@ -1,6 +1,7 @@
 package query
 
 import (
+	"errors"
 	"fmt"
 	"maps"
 	"strconv"
@@ -14,6 +15,19 @@ const (
 	KeyWhere = "{{.where}}"
 )
 
+type PagingResult[T any] struct {
+	Page      int
+	PageSize  int
+	TotalPage int64
+	Total     int64
+	Error     error `json:",omitempty"`
+	Data      []T   `json:",omitempty"`
+}
+
+var (
+	PageSize = 100 // default page size
+)
+
 // it's not enabled yet.
 type RawQueryWhere struct {
 	Cond  string
@@ -22,16 +36,150 @@ type RawQueryWhere struct {
 }
 
 type RawQuery struct {
-	Sql     string
-	Preset  map[string]any
-	Params  []string
-	Where   map[string]string // key: where condition, value: param key, e.g. "id = ?", "id"
-	Orderby string
-	Groupby string
+	Sql        string
+	SumRef     string // reference to another RawQuery define, not used yet.
+	SumEnabled bool
+	Preset     map[string]any
+	Params     []string
+	Where      map[string]string // key: where condition, value: param key, e.g. "id = ?", "id"
+	Orderby    string
+	Groupby    string
 }
 
 func (r *RawQuery) Query(db *gorm.DB, data map[string]any) ([]map[string]any, error) {
 	return Query[map[string]any](db, r, data)
+}
+
+// check should return Paging Result
+func (r *RawQuery) ShouldPagingResult() bool {
+	return r.SumRef != "" || r.SumEnabled
+}
+
+func (r *RawQuery) PagingResult(db *gorm.DB, result []map[string]any, req map[string]any) (*PagingResult[map[string]any], error) {
+	pageSize := toInt(req, "page_size")
+	page := toInt(req, "page")
+	if pageSize == 0 {
+		pageSize = PageSize
+	}
+
+	resp := &PagingResult[map[string]any]{
+		Page:     page,
+		PageSize: pageSize,
+		Data:     result,
+		Total:    int64(len(result)),
+	}
+
+	if len(result) == 0 {
+		resp.TotalPage = 0
+		return resp, nil
+	}
+
+	if len(result) < pageSize {
+		resp.TotalPage = 1
+		return resp, nil
+	}
+	total, err := r.sum(db, req)
+	if err != nil {
+		return nil, err
+	}
+	pageTotal := (total + int64(pageSize-1)) / int64(pageSize)
+
+	resp.TotalPage = pageTotal
+	return resp, nil
+}
+
+func (r *RawQuery) where(allParams map[string]any, params []any, sql string) (string, []any, error) {
+	conds := make([]string, 0, len(r.Where))
+	for p, cond := range r.Where {
+		if v, ok := allParams[p]; ok {
+			if s, ok := v.(string); ok {
+				ss := strings.TrimSpace(s)
+				if ss == "" || ss == "null" || ss == "-" || ss == "none" {
+					zap.L().Debug("empty value ", zap.String("param", p))
+					continue
+				}
+				v = ss
+			}
+			conds = append(conds, cond)
+			if strings.Contains(cond, "like") {
+				s, ok := v.(string)
+				if !ok {
+					return sql, params, errors.New("like param must be string")
+				}
+				if !strings.HasPrefix(s, "%") {
+					s = s + "%"
+				}
+				params = append(params, s)
+			} else {
+				params = append(params, v)
+			}
+
+		}
+	}
+	if len(conds) > 0 {
+		conds := strings.Join(conds, " and ")
+		if strings.Contains(sql, KeyWhere) {
+			sql = strings.Replace(sql, KeyWhere, conds, 1)
+		} else {
+			sql = sql + " where " + conds
+		}
+	}
+	return sql, params, nil
+}
+
+func (r *RawQuery) sum(db *gorm.DB, data map[string]any) (int64, error) {
+	allParams := map[string]any{}
+	maps.Copy(allParams, r.Preset)
+	maps.Copy(allParams, data)
+
+	params := make([]any, 0)
+	for _, key := range r.Params {
+		params = append(params, allParams[key])
+	}
+	// sql := r.SumSql
+	// if sql == "" {
+	// start:=strings.Index(r.Sql, "select ")
+	end := strings.Index(r.Sql, "from")
+	sql := "select count(1) as sum " + r.Sql[end:]
+	if len(r.Where) > 0 {
+		var err error
+		sql, params, err = r.where(allParams, params, sql)
+		if err != nil {
+			return 0, err
+		}
+		// conds := make([]string, 0)
+		// for p, cond := range r.Where {
+		// 	if v, ok := allParams[p]; ok {
+		// 		if s, ok := v.(string); ok {
+		// 			ss := strings.TrimSpace(s)
+		// 			if ss == "" || ss == "null" || ss == "-" || ss == "none" {
+		// 				zap.L().Debug("empty value ", zap.String("param", p))
+		// 				continue
+		// 			}
+		// 		}
+		// 		conds = append(conds, cond)
+		// 		params = append(params, v)
+		// 	}
+		// }
+		// if len(conds) > 0 {
+		// 	conds := strings.Join(conds, " and ")
+		// 	if strings.Contains(sql, KeyWhere) {
+		// 		sql = strings.Replace(sql, KeyWhere, conds, 1)
+		// 	} else {
+		// 		sql = sql + " where " + conds
+		// 	}
+		// }
+	}
+	// }
+
+	count := int64(0)
+	tx := db.Raw(sql, params...)
+
+	err := tx.Find(&count).Error
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
 }
 
 func Query[T any](db *gorm.DB, r *RawQuery, data map[string]any) ([]T, error) {
@@ -48,28 +196,33 @@ func Query[T any](db *gorm.DB, r *RawQuery, data map[string]any) ([]T, error) {
 	}
 
 	if len(r.Where) > 0 {
-		conds := make([]string, 0)
-		for p, cond := range r.Where {
-			if v, ok := allParams[p]; ok {
-				if s, ok := v.(string); ok {
-					ss := strings.TrimSpace(s)
-					if ss == "" || ss == "null" || ss == "-" || ss == "none" {
-						zap.L().Debug("empty value ", zap.String("param", p))
-						continue
-					}
-				}
-				conds = append(conds, cond)
-				params = append(params, v)
-			}
+		var err error
+		sql, params, err = r.where(allParams, params, sql)
+		if err != nil {
+			return nil, err
 		}
-		if len(conds) > 0 {
-			conds := strings.Join(conds, " and ")
-			if strings.Contains(sql, KeyWhere) {
-				sql = strings.Replace(sql, KeyWhere, conds, 1)
-			} else {
-				sql = sql + " where " + conds
-			}
-		}
+		// conds := make([]string, 0)
+		// for p, cond := range r.Where {
+		// 	if v, ok := allParams[p]; ok {
+		// 		if s, ok := v.(string); ok {
+		// 			ss := strings.TrimSpace(s)
+		// 			if ss == "" || ss == "null" || ss == "-" || ss == "none" {
+		// 				zap.L().Debug("empty value ", zap.String("param", p))
+		// 				continue
+		// 			}
+		// 		}
+		// 		conds = append(conds, cond)
+		// 		params = append(params, v)
+		// 	}
+		// }
+		// if len(conds) > 0 {
+		// 	conds := strings.Join(conds, " and ")
+		// 	if strings.Contains(sql, KeyWhere) {
+		// 		sql = strings.Replace(sql, KeyWhere, conds, 1)
+		// 	} else {
+		// 		sql = sql + " where " + conds
+		// 	}
+		// }
 	}
 
 	groupby := r.Groupby
