b6dc14030b5019aae7d7dc005c81d6bbf7891cd7
PanArm <panarm@esquel.com>
2026-01-17 00:24:08 +0800

    try replace with driver only.

--- pkg/orm/odbc.go
+++ pkg/orm/odbc.go
@@ -1,35 +1,43 @@
 package orm
 
 import (
-	"database/sql"
 	"strings"
 
 	_ "github.com/alexbrainman/odbc"
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 )
 
 func regWithOdbc(typeName string) {
 	dbtype := strings.TrimSuffix(typeName, ".odbc")
-	DialectorMap[dbtype] = func(dsn string) gorm.Dialector {
-		db, err := sql.Open("odbc", dsn)
-		if err != nil {
-			panic(err)
-		}
+	DialectorMap[typeName] = func(dsn string) gorm.Dialector {
+		// db, err := sql.Open("odbc", dsn)
+		// if err != nil {
+		// 	panic(err)
+		// }
 		switch dbtype {
 		case "sqlserver":
 			return sqlserver.Dialector{
 				Config: &sqlserver.Config{
-					DSN:  dsn,
-					Conn: db,
+					DSN:        dsn,
+					DriverName: "odbc",
+				},
+			}
+		case "mysql":
+			return mysql.Dialector{
+				Config: &mysql.Config{
+					DSN:        dsn,
+					DriverName: "odbc",
 				},
 			}
 		default:
-			panic("not support dbtype")
+			panic("not support dbtype: " + dbtype)
 		}
 	}
 }
 
 func init() {
+	regWithOdbc("mysql.odbc")
 	regWithOdbc("sqlserver.odbc")
 }
